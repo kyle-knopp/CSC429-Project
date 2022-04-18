06941193c354ef5496761c6b87a8288ef278a79f
Padraic <sonicalmx1@gmail.com>
2022-04-18 15:46:55 -0400

    Transaction class shells

---
+++ ProjectTemplate/ImplementationFX/model/ListStudentBorrowersWithBookCheckedOutTransaction.java
@@ -0,0 +1,109 @@
+package model;
+
+// system imports
+import javafx.scene.Scene;
+
+import java.util.Enumeration;
+import java.util.Properties;
+
+// project imports
+import event.Event;
+import exception.InvalidPrimaryKeyException;
+
+import userinterface.View;
+import userinterface.ViewFactory;
+
+public class ListStudentBorrowersWithBookCheckedOutTransaction extends Transaction{
+    private StudentBorrowerCollection myStudents;
+
+    private String transactionErrorMessage = "";
+
+    /**
+     * Constructor for this class.
+     *
+     *
+     */
+    //----------------------------------------------------------
+    public ListStudentBorrowersWithBookCheckedOutTransaction() throws Exception
+    {
+        super();
+    }
+
+    //----------------------------------------------------------
+    protected void setDependencies()
+    {
+        dependencies = new Properties();
+
+        dependencies.setProperty("Cancel", "CancelTransaction");
+
+        myRegistry.setDependencies(dependencies);
+    }
+
+    /**
+     * This method encapsulates all the logic of creating the account,
+     * verifying ownership, crediting, etc. etc.
+     */
+    //----------------------------------------------------------
+    public void processTransaction(Properties props)
+    {
+
+
+    }
+
+    @Override
+    protected Scene createView() {
+        Scene currentScene = myViews.get(""); // change to whatever view we will create
+
+        if (currentScene == null) {
+            View newView = ViewFactory.createView("", this);
+            currentScene = new Scene(newView);
+            myViews.put("", currentScene);
+        }
+
+        return currentScene;
+    }
+
+    private void createAndShowView() {  // change to whatever view we will create
+        //transactionErrorMessage = "";
+        Scene currentScene = (Scene) myViews.get("");
+        if (currentScene == null) {
+            View newView = ViewFactory.createView("", this);
+            currentScene = new Scene(newView);
+
+
+            myViews.put("", currentScene);
+        }
+
+        swapToView(currentScene);
+    }
+
+    //-----------------------------------------------------------
+    public Object getState(String key)
+    {
+        if (key.equals("TransactionError") == true)
+        {
+            return transactionErrorMessage;
+        }
+        else if (key.equals("UpdateStatusMessage") == true)
+        {
+            return transactionErrorMessage;
+        }
+        return null;
+    }
+
+    //-----------------------------------------------------------
+    public void stateChangeRequest(String key, Object value)
+    {
+        // DEBUG System.out.println("DepositTransaction.sCR: key: " + key);
+
+        if (key.equals("DoYourJob") == true)
+        {
+            doYourJob();
+        }
+
+        myRegistry.updateSubscribers(key, this);
+    }
+
+}
+
+
